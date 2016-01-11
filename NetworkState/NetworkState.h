c185cfa028a690edc6975822d22a25427b43ba8d
Gareth Jones <gareth@garethpaul.com>
2016-01-10 20:18:46 -0800

    adding network state initial commit

---
+++ NetworkState/NetworkState.h
@@ -0,0 +1,19 @@
+//
+//  NetworkState.h
+//  NetworkState
+//
+//  Created by Gareth Jones on 1/10/16.
+//  Copyright © 2016 GPJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkState.
+FOUNDATION_EXPORT double NetworkStateVersionNumber;
+
+//! Project version string for NetworkState.
+FOUNDATION_EXPORT const unsigned char NetworkStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkState/PublicHeader.h>
+
+
