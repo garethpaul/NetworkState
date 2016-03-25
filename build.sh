e85354db4d36e4e17f75d3be7341e85440594ee3
Gareth Jones <gareth@garethpaul.com>
2016-03-24 20:00:00 -0600

    adding build changes for tests

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -eu
 
 function ci_lib() {
     NAME=$1
-    xcodebuild -project NetworkState.xcodeproj
+    xcodebuild -project NetworkState.xcodeproj \
+               -scheme "NetworkStateTests" \
+               -destination "platform=iOS Simulator,name=${NAME}" \
+               -sdk iphonesimulator \
+               build test
 }
-
 ci_lib "iPhone 5"
